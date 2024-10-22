6f7b1a242d7b2b2917a3f4891b5686a8bc3b8ea9
Ruslan Babayev <rus.babaef@yandex.ru>
2024-10-22 14:34:04 +0300

    - fix test workflow

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,2 @@
-from fastapi.testclient import TestClient
 
-from sample_notes import app
-
-client = TestClient(app)
-
-
-def test_health():
-    response = client.get("/health_check/ping")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Pong!"}
-
-
-def test_login_admin():
-    response = client.get("/health_check/ping")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Pong!"}
-
-
-def test_login_user():
-    response = client.get("/health_check/ping")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Pong!"}
 
